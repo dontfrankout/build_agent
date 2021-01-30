e9430c8e73c56c6c9f3f149ac1ff76b6614eb982
Frank Meyer <fmeyer@impact-xm.com>
2021-01-30 17:07:36 -0500

    init

---
+++ config.js
@@ -0,0 +1,14 @@
+export const config = {
+
+    secret: 'freddiebeans',
+
+    repos: [
+
+        {name: 'xm_budget', branch: 'main', deploy_command: 'sudo touch /var/www/html/budget/deploy.yay && '},
+
+    ]
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+import http from 'http';
+import crypto from 'crypto';
+import {config} from './config.js';
+//import { exec } from 'child_process';
+ 
+const SECRET = 'MY_GITHUB_WEBHOOK_SECRET';
+ 
+const GITHUB_REPOSITORIES_TO_DIR = {
+  'dontfrankout/xm_budget': '/home/ec2-user/xm_budget',
+};
+ 
+http
+  .createServer((req, res) => {
+    req.on('data', chunk => {
+      console.log("in on data")
+      const signature = `sha1=${crypto
+        .createHmac('sha1', SECRET)
+        .update(chunk)
+        .digest('hex')}`;
+ 
+      const isAllowed = req.headers['x-hub-signature'] === signature;
+ 
+      const body = JSON.parse(chunk);
+ 
+      //const isMaster = body.ref === 'refs/heads/master';
+      
+      const directory = GITHUB_REPOSITORIES_TO_DIR[body.repository.full_name];
+ 
+      if (isAllowed && directory) {
+        try {
+          console.log("do something already")
+          //exec(`cd ${directory} && bash deploy.sh`);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+ 
+    res.end();
+  })
+  .listen(8080);
