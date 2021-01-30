b3d2f37e7a52b443b6af0c0cece2d8f106850f93
Frank Meyer <fmeyer@impact-xm.com>
2021-01-30 17:21:10 -0500

    auto

--- index.js
+++ index.js
@@ -8,34 +8,36 @@ const SECRET = 'MY_GITHUB_WEBHOOK_SECRET';
 const GITHUB_REPOSITORIES_TO_DIR = {
   'dontfrankout/xm_budget': '/home/ec2-user/xm_budget',
 };
+
+const requestListener = function (req, res) {
+  req.on('data', chunk => {
+    console.log("in on data")
+    const signature = `sha1=${crypto
+      .createHmac('sha1', SECRET)
+      .update(chunk)
+      .digest('hex')}`;
+
+    const isAllowed = req.headers['x-hub-signature'] === signature;
+
+    const body = JSON.parse(chunk);
+
+    //const isMaster = body.ref === 'refs/heads/master';
+    
+    const directory = GITHUB_REPOSITORIES_TO_DIR[body.repository.full_name];
+
+    if (isAllowed && directory) {
+      try {
+        console.log("do something already")
+        //exec(`cd ${directory} && bash deploy.sh`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+  res.writeHead(200);
+  res.end("hello?");
+}
  
 http
-  .createServer((req, res) => {
-    req.on('data', chunk => {
-      console.log("in on data")
-      const signature = `sha1=${crypto
-        .createHmac('sha1', SECRET)
-        .update(chunk)
-        .digest('hex')}`;
- 
-      const isAllowed = req.headers['x-hub-signature'] === signature;
- 
-      const body = JSON.parse(chunk);
- 
-      //const isMaster = body.ref === 'refs/heads/master';
-      
-      const directory = GITHUB_REPOSITORIES_TO_DIR[body.repository.full_name];
- 
-      if (isAllowed && directory) {
-        try {
-          console.log("do something already")
-          //exec(`cd ${directory} && bash deploy.sh`);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    });
- 
-    res.end("hello?");
-  })
+  .createServer(requestListener)
   .listen(8080);
